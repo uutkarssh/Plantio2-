c4d21c561e682e7b30920ffaef164112321d137b
Utkarsh Maurya <utkarshmaurya88409@gmail.com>
2026-08-10 17:02:02 +0530

    Replace Supabase auth with Firebase auth config

---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRZczZyqxzO_pIgmXhIdaNM7xL6IcB-rY",
+  authDomain: "plantio-c3882.firebaseapp.com",
+  projectId: "plantio-c3882",
+  storageBucket: "plantio-c3882.firebasestorage.app",
+  messagingSenderId: "440090239604",
+  appId: "1:440090239604:web:27dfea254ce18cf434a721",
+  measurementId: "G-957QW77DPM",
+};
+
+export const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
